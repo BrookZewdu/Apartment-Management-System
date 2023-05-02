8c64d07d684c99084beb2bef657d8fa50a16e985
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-05-02 23:20:13 +0300

    update auth service (#5)

--- starter_apartement/src/services/authService.js
+++ starter_apartement/src/services/authService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const login = async (email, password) => {
-  return axios.post("/users/login", { email, password }).then((response) => {
-    return response.data.user;
-  });
+    return axios.post("/users/login", { email, password }).then((response) => {
+        return response.data.data;
+    });
 };
 
-export const register = async (formData) => {
-  return axios.post("/users/signup", formData).then((response) => {
-    return response.data.user;
-  });
+export const register = async (firstName, lastName, telegramUrl, email, password) => {
+    return axios.post("/users/signup", { name: firstName, lastname: lastName, telegram: telegramUrl, email, password }).then((response) => {
+        return response.data.data;
+    });
 };
