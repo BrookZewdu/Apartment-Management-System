a415b1d589a1d497b8e1f2e4aac1afaf5e646f58
yosefalemu <yosefalemumengstie@gmail.com>
2023-05-02 23:17:04 +0300

    update auth service

--- starter_apartement/src/services/authService.js
+++ starter_apartement/src/services/authService.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const login = async (email, password) => {
     return axios.post("/users/login", { email, password }).then((response) => {
-        return response.data.user;
+        return response.data.data;
     });
 };
 
 export const register = async (firstName, lastName, telegramUrl, email, password) => {
     return axios.post("/users/signup", { name: firstName, lastname: lastName, telegram: telegramUrl, email, password }).then((response) => {
-        return response.data.user;
+        return response.data.data;
     });
 };
