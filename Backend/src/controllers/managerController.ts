2d7714b342189f28afa46fce900bb5eae0b56bf1
Biruk Zewdu <Brookzewdu09@gmail.com>
2023-05-08 02:48:44 +0300

    Revert "Revert "pulled all the backend changes from Mike's separate repo""
    This reverts commit 4319afd3e832101491294f6f28186d3ce5f435b0.

--- Backend/src/controllers/managerController.ts
+++ Backend/src/controllers/managerController.ts
@@ -3,6 +3,7 @@ import User, { IUser } from "../models/User";
 import { LeaseAgreement, ILeaseAgreement } from "../models/LeaseAgreement";
 import Apartment, { IApartment } from "../models/apartment";
 import ApartmentRequest, { IApartmentRequest } from "../models/registerRequest";
+import Mentainance, { IMentainance } from "../models/mentainance";
 
 
 // get all tenants
@@ -93,11 +94,13 @@ export const acceptApartmentRequest = async (
       user: user._id,
       startDate: req.body.startDate,
       endDate: req.body.endDate,
+      rent: apartment.price
     };
     const newLease = await LeaseAgreement.create(lease) as ILeaseAgreement;
     user.isTenant = true;
     await user.save();
     apartment.available = false;
+    apartment.occupants = user._id
     await apartment.save();
     apartmentRequest.status = "accepted";
     await apartmentRequest.save();
@@ -123,6 +126,111 @@ export const rejectApartmentRequest = async (
     res.status(200).json({ success: true, data: apartmentRequest });
   }
   catch(error){
+    res.status(400).json({ success: false, data: (error as Error).message });
+  }
+}
+
+// delete lease agreement
+
+export const deleteLeaseAgreement = async (
+  req:Request,
+  res:Response
+) => {
+  try {
+    const lease = await LeaseAgreement.findByIdAndDelete(req.params.id) as ILeaseAgreement
+    const apartment = await Apartment.findById(lease.apartment)
+    const user = await User.findById(lease.user)
+    
+    if (!apartment || !user){
+      return res.status(404).json({
+        success:false,
+        data:"apartment or user not found"
+      })
+    }
+    apartment.available = true
+    apartment.occupants = null
+    user.isTenant = false
+  }
+  catch(error){
+    return res.status(400).json({
+      success:false,
+      data:"error"
+    })
+  }
+}
+
+// get all apartment requests
+export const getAllApartmentRequests = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const apartmentRequests: IApartmentRequest[] = await ApartmentRequest.find().populate("user").populate("apartment");
+    res.status(200).json({ success: true, data: apartmentRequests });
+  } catch (error) {
+    res.status(400).json({ success: false, data: (error as Error).message });
+  }
+}
+
+// get all accepted apartment requests
+export const getAllAcceptedApartmentRequests = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const apartmentRequests: IApartmentRequest[] = await ApartmentRequest.find({status: "accepted"}).populate("user").populate("apartment");
+    res.status(200).json({ success: true, data: apartmentRequests });
+  } catch (error) {
+    res.status(400).json({ success: false, data: (error as Error).message });
+  }
+}
+
+// get all rejected apartment requests
+export const getAllRejectedApartmentRequests = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const apartmentRequests: IApartmentRequest[] = await ApartmentRequest.find({status: "rejected"}).populate("user").populate("apartment");
+    console.log(apartmentRequests)
+    res.status(200).json({ success: true, data: apartmentRequests });
+  } catch (error) {
+    res.status(400).json({ success: false, data: (error as Error).message });
+  }
+}
+
+// get all lease agreements
+
+export const getAllLeaseAgreements = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const leaseAgreements: ILeaseAgreement[] = await LeaseAgreement.find().populate("user").populate("apartment");
+    if(!leaseAgreements){
+      res.status(400).json({success:true, data:'lease agreement not found'})
+    }
+    res.status(200).json({ success: true, data: leaseAgreements });
+  } catch (error) {
+    res.status(400).json({ success: false, data: (error as Error).message });
+  }
+}
+
+//get lease agreement by apartment id
+export const getLeaseAgreementByApartmentId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const leaseAgreement = await LeaseAgreement.findOne({apartment: req.body.apartment_id}).populate("user").populate("apartment");
+    res.status(200).json({ success: true, data: leaseAgreement });
+  }
+  catch (error) {
     res.status(400).json({ success: false, data: (error as Error).message });
   }
 }
--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,25 +1,28 @@
-// all user routes
 import express from 'express';
-import { signup, completeSignup,login,logout, forgotPassword,resetPassword,getAllUsers,
-    updateDetails,updatePassword,getSingleUser,deleteUser,makeApartmentRequest, getApplications, applyForApartment
+import { signup, completeSignup,login,logout, forgotPassword,resetPassword,
+    updateDetails,updatePassword,makeApartmentRequest,cancelApartmentRequest,addVisitor
  } from '../controllers/userControllers';
+ import { createMentainanceRequest } from '../controllers/mentainanceControllers';
+ import { isAuthenticatedUser, authorizeRoles } from "../authentication/auth";
 
 
 
 const router = express.Router();
 
 router.route('/signup').post(signup);
-router.route('/verify-email/:token').get(completeSignup);
+router.route('/completesignup/:token').get(completeSignup);
 router.route('/login').post(login);
-router.route('/logout').get(logout);
+router.route('/logout').get(isAuthenticatedUser,logout);
 router.route('/forgotpassword').post(forgotPassword);
 router.route('/resetpassword/:resetToken').put(resetPassword);
-router.route('/').get(getAllUsers);
-router.route('/updatedetails').put(updateDetails);
-router.route('/updatepassword').put(updatePassword);
-router.route('/applications').get(getApplications);
-router.route('/apply/:apartment_id').post(applyForApartment);
+router.route('/updatedetails').put(isAuthenticatedUser,updateDetails);
+router.route('/updatepassword').put(isAuthenticatedUser,updatePassword);
+router.route('/makeApartmentRequest').post(isAuthenticatedUser,makeApartmentRequest);
+router.route('/cancelApartmentRequest').put(isAuthenticatedUser,cancelApartmentRequest);
+router.route('/addVisitor').put(isAuthenticatedUser,addVisitor);
+router.route('/maintenanceRequest').post(createMentainanceRequest);
+
+
+
 
 export default router;
-
-
