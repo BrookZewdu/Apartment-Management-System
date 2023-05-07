ebc24b3aff166a1dbdb3d757b678c8494657d434
Biruk Zewdu <Brookzewdu09@gmail.com>
2023-05-08 02:25:34 +0300

    pulled all the backend changes from Mike's separate repo

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,13 +3,19 @@ import { connectToDatabase } from './config/database';
 import cloudinary from 'cloudinary';
 import * as dotenv from 'dotenv';
 import UserRoutes from './routes/userRoutes';
+import ApartmentRoutes from './routes/apartmentRoutes';
+import CommonRoutes from './routes/commonRoutes';
+import GuardRoutes from './routes/guardRoutes';
+import OwnerRoutes from './routes/ownerRoutes';
+import ManagerRoutes from './routes/managerRoutes';
 import cookieParser from 'cookie-parser';
 import fileupload from 'express-fileupload';
 import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -20,11 +26,21 @@ cloudinary.v2.config({
 connectToDatabase();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
+));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileupload());
 app.use('/api/users', UserRoutes);
+app.use('/api/apartments', ApartmentRoutes);
+app.use('/api/common', CommonRoutes);
+app.use('/api/guard', GuardRoutes);
+app.use('/api/owner', OwnerRoutes);
+app.use('/api/manager', ManagerRoutes);
 
 
 app.listen(port, () => {
--- Backend/src/routes/managerRoutes.ts
+++ Backend/src/routes/managerRoutes.ts
@@ -1,13 +1,25 @@
 import { isAuthenticatedUser, authorizeRoles } from "../authentication/auth";
 import { authorizeRoleChange } from "../authentication/rolecontrolls";
-import { acceptApartmentRequest, getAllSecurityGuards, makeSecurityGuard,rejectApartmentRequest } from "../controllers/managerController";
+import { acceptApartmentRequest, getAllSecurityGuards, makeSecurityGuard,
+    rejectApartmentRequest,deleteLeaseAgreement,getAllAcceptedApartmentRequests,getAllRejectedApartmentRequests,getAllApartmentRequests,getLeaseAgreementByApartmentId,getAllLeaseAgreements } from "../controllers/managerController";
 import express from 'express';
-
+import { getAllMentainanceRequests,getAllAcceptedMentainanceRequests ,getAllRejectedMentainanceRequests,AcceptMentainanceRequests,RejectMentainanceRequests} from "../controllers/mentainanceControllers";
 const router = express.Router();
 
-router.route('/allSecurityGuards').get(isAuthenticatedUser,authorizeRoles('manager'),getAllSecurityGuards);
-router.route('/makesSecurityGuard').put(isAuthenticatedUser,authorizeRoles('manager'),authorizeRoleChange,makeSecurityGuard);
-router.route('/acceptApartmentRequest/:id').put(isAuthenticatedUser,authorizeRoles('manager'),authorizeRoleChange,acceptApartmentRequest);
-router.route('/rejectApartmentRequest/:id').put(isAuthenticatedUser,authorizeRoles('manager'),authorizeRoleChange,rejectApartmentRequest);
+router.route('/allSecurityGuards').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllSecurityGuards);
+router.route('/makesSecurityGuard').put(isAuthenticatedUser,authorizeRoles('manager','owner'),authorizeRoleChange,makeSecurityGuard);
+router.route('/acceptApartmentRequest/:id').put(isAuthenticatedUser,authorizeRoles('manager','owner'),acceptApartmentRequest);
+router.route('/rejectApartmentRequest/:id').put(isAuthenticatedUser,authorizeRoles('manager','owner'),rejectApartmentRequest);
+router.route('/deleteLeaseAgreement/:id').delete(isAuthenticatedUser,authorizeRoles('manager','owner'),deleteLeaseAgreement);
+router.route('/getAllApartmentRequests').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllApartmentRequests);
+router.route('/getAllAcceptedApartmentRequests').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllAcceptedApartmentRequests);
+router.route('/getAllRejectedApartmentRequests').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllRejectedApartmentRequests);
+router.route('/maintenanceRequest').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllMentainanceRequests);
+router.route('/maintenanceRequest/accepted').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllAcceptedMentainanceRequests)
+router.route('/maintenanceRequest/accepted/:id').put(isAuthenticatedUser,authorizeRoles('manager','owner'),AcceptMentainanceRequests)
+router.route('/maintenanceRequest/rejected').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllRejectedMentainanceRequests)
+router.route('/maintenanceRequest/rejected/:id').put(isAuthenticatedUser,authorizeRoles('manager','owner'),RejectMentainanceRequests);
+router.route('/leaseAgreement').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getLeaseAgreementByApartmentId);
+router.route('/getAllLeaseAgreement').get(isAuthenticatedUser,authorizeRoles('manager','owner'),getAllLeaseAgreements);
 
 export default router;
